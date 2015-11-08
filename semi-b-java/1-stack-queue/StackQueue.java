94b40aaed992e83a84d87bde3eaac8f2e5e5398c
Toru Hisai <toru@torus.jp>
2015-11-08 13:03:40 +0900

    add keyboard

--- semi-b-java/1-stack-queue/StackQueue.java
+++ semi-b-java/1-stack-queue/StackQueue.java
@@ -1,10 +1,21 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import java.awt.Container;
-import java.awt.BorderLayout;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 
 // 参考サイト: http://www.javadrive.jp/tutorial/jbutton/index1.html
+
+class DentakuActionListener implements ActionListener {
+    Queue inputs;
+    DentakuActionListener(Queue q) {
+	this.inputs = q;
+    }
+
+    public void actionPerformed(ActionEvent ev) {
+	JButton btn = (JButton)ev.getSource();
+	System.out.println(btn.getText());
+	this.inputs.enqueue(btn.getText());
+    }
+}
 
 class StackQueue extends JFrame{
     public static void main(String args[]){
@@ -13,23 +24,43 @@ class StackQueue extends JFrame{
 
     }
 
+    private JPanel makeRow(ActionListener al, String... labels) {
+	JPanel panel = new JPanel();
+
+	for (int i = 0; i < labels.length; i ++) {
+	    JButton btn = new JButton(labels[i]);
+	    btn.addActionListener(al);
+	    panel.add(btn);
+	}
+
+	return panel;
+    }
+
     StackQueue(String title){
 	setTitle(title);
 	setBounds(100, 100, 300, 250);
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	JPanel p = new JPanel();
-
-	JButton button1 = new JButton();
-	JButton button2 = new JButton("確認");
-	JButton button3 = new JButton();
-	button3.setText("キャンセル");
-
-	p.add(button1);
-	p.add(button2);
-	p.add(button3);
-
 	Container contentPane = getContentPane();
-	contentPane.add(p, BorderLayout.CENTER);
+
+	JPanel console = new JPanel();
+	console.setLayout(new BoxLayout(console, BoxLayout.Y_AXIS));
+
+	JLabel display = new JLabel("");
+
+	JPanel keyboard = new JPanel();
+
+	Queue inputs = new Queue();
+
+	ActionListener al = new DentakuActionListener(inputs);
+
+	keyboard.add(makeRow(al, "7", "8", "9", "*"));
+	keyboard.add(makeRow(al, "4", "5", "6", "/"));
+	keyboard.add(makeRow(al, "1", "2", "3", "+"));
+	keyboard.add(makeRow(al, "P", "0", "E", "-"));
+
+	console.add(display);
+	console.add(keyboard);
+	contentPane.add(console);
     }
 }
