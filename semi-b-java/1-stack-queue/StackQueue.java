9f787df11026c52973c9e00e5cd97772a08b29af
Toru Hisai <toru@torus.jp>
2015-11-08 15:18:49 +0900

    add some operations

--- semi-b-java/1-stack-queue/StackQueue.java
+++ semi-b-java/1-stack-queue/StackQueue.java
@@ -17,6 +17,69 @@ class DentakuActionListener implements ActionListener {
     }
 }
 
+class DentakuTimerListener implements ActionListener {
+    Queue inputs;
+    Stack stack;
+    JLabel display;
+
+    DentakuTimerListener(Queue q, Stack s, JLabel d) {
+	this.inputs = q;
+	this.stack = s;
+	this.display = d;
+    }
+
+    public void actionPerformed(ActionEvent ev) {
+	if (!this.inputs.empty()) {
+	    Object elem = this.inputs.dequeue();
+	    if (elem == "E") {
+		System.out.println("ENTER");
+		double num = 0;
+		double weight = 1;
+		while (!this.stack.empty() && this.stack.top() instanceof String) {
+		    num += weight * Double.parseDouble((String)this.stack.pop());
+		    weight *= 10;
+		}
+		this.stack.push(num);
+	    } else if (elem == "*") {
+		System.out.println("MULTIPLY");
+		double a = (double)this.stack.pop();
+		double b = (double)this.stack.pop();
+		this.stack.push(a * b);
+	    } else if (elem == "+") {
+		System.out.println("PLUS");
+		double a = (double)this.stack.pop();
+		double b = (double)this.stack.pop();
+		this.stack.push(a + b);
+	    } else if (elem == "-") {
+		System.out.println("MINUS");
+		double a = (double)this.stack.pop();
+		double b = (double)this.stack.pop();
+		this.stack.push(b - a);
+	    } else if (elem == "/") {
+		System.out.println("DIVIDE");
+		double a = (double)this.stack.pop();
+		double b = (double)this.stack.pop();
+		this.stack.push(b / a);
+	    } else if (elem == "P") {
+		if (!this.stack.empty()) {
+		    System.out.println("POP");
+		    this.stack.pop();
+		}
+	    } else {
+		this.stack.push(elem);
+	    }
+
+	    if (this.stack.empty()) {
+		this.display.setText("EMPTY");
+	    } else {
+		Object top = this.stack.top();
+		System.out.println(top);
+		this.display.setText(top.toString());
+	    }
+	}
+    }
+}
+
 class StackQueue extends JFrame{
     public static void main(String args[]){
 	StackQueue frame = new StackQueue("タイトル");
@@ -46,12 +109,9 @@ class StackQueue extends JFrame{
 	JPanel console = new JPanel();
 	console.setLayout(new BoxLayout(console, BoxLayout.Y_AXIS));
 
-	JLabel display = new JLabel("");
-
+	JLabel display = new JLabel("HELLO");
 	JPanel keyboard = new JPanel();
-
 	Queue inputs = new Queue();
-
 	ActionListener al = new DentakuActionListener(inputs);
 
 	keyboard.add(makeRow(al, "7", "8", "9", "*"));
@@ -62,5 +122,9 @@ class StackQueue extends JFrame{
 	console.add(display);
 	console.add(keyboard);
 	contentPane.add(console);
+
+	Stack stack = new Stack();
+	Timer timer = new Timer(400, new DentakuTimerListener(inputs, stack, display));
+	timer.start();
     }
 }
